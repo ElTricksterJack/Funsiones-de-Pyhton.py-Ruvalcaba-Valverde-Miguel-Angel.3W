e26d278584c5e8d87d38bc8317adc2cd5c9c6b4f
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-10-30 16:51:03 -0600

    Update codigo-1.py

--- codigo-1.py
+++ codigo-1.py
@@ -1,1 +1,14 @@
-
+print("\nNombre: Ruvalcaba Valverde Miguel Angel")
+print("----------------Funciones-----------------")
+print("Que es un funsion: Una función es un bloque de código que solo se ejecuta cuando se lo llama. \nPuede pasar datos, conocidos como parámetros, a una función. \nUna función puede devolver datos como resultado.")
+#Uf tremendo copy paste, pero mejor lo explicare con mis propias palabras.
+#Una funsion es un valor que tiene codigo ya asignao que puedes invocar a tu gusto, el ejemplo esta esta acontinuaion.
+print("-----")
+def FEDEX():#Funsion Experimetal Del Espesimen Xipetotec
+  x = 12412+123
+  print("One piece y undead unluck son lo mejor")
+  print(x) #aqui esta el codigo de la funsion.
+FEDEX()
+#aqui invocamos el codigo de la funsion FEDX
+print("--------------------------------------")
+print("Resultado: se se mostro el mensaje\n")

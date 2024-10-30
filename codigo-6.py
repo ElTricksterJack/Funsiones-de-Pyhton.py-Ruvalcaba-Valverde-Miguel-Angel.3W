de1cbc9eb816dfb49f4de64b3dc691adb8baf376
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-10-30 17:04:52 -0600

    Update codigo-6.py

--- codigo-6.py
+++ codigo-6.py
@@ -1,1 +1,8 @@
-
+print("\nNombre: Ruvalcaba Valverde Miguel Angel")
+print("----Argumentos arbitrarios, *args-----")
+def YJ(*vi):#YoJo
+  print("yo juego " + vi[2])#se cuenta inisiando desde el 0
+#amens que... si hay varias opsiones y programas el argumento de manera antisipada, si funsiona
+YJ("Minegraft", "Roblox", "Dislyte")
+print("--------------------------------------")
+print("Resultado: se eligio la opcion predeterminada.\n")

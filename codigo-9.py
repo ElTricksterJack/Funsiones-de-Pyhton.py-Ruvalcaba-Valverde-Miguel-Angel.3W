a68b047e8564106541c9f71441b19cc200d18e0a
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-10-30 17:10:42 -0600

    Update codigo-9.py

--- codigo-9.py
+++ codigo-9.py
@@ -1,1 +1,14 @@
-
+print("\nNombre: Ruvalcaba Valverde Miguel Angel")
+print("----Valor del parámetro predeterminado----")
+def my_function(country = "soy Goku"):
+  print("yo soy " + country)
+my_function("tu pader")
+print("Y")
+my_function("inevitable")
+my_function()
+print("Nota:Para entender vea el video porfabor.")
+import webbrowser
+webbrowser.open("https://www.youtube.com/shorts/wzob3c7fyiM")#https://www.youtube.com/shorts/wzob3c7fyiM
+True
+#si invesgie esto solo para que lo viera
+#con esto se demuestra como podermos acumular cambiar argumentos y si hay un espasio disponible para el argumento predifinido se podra integrar

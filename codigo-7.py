8b373ce00d1ca00761fd1567628f2e2142001d55
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-10-30 17:07:47 -0600

    Update codigo-7.py

--- codigo-7.py
+++ codigo-7.py
@@ -1,1 +1,7 @@
-
+print("\nNombre: Ruvalcaba Valverde Miguel Angel")
+def my_function(ch3, ch2, ch1):
+  print("tu negador es " + ch3)
+#qui se ben las elesiones pero se elige solo 1
+my_function(ch1 = "unseen", ch2 = "unfeel", ch3 = "unluck")
+print("--------------------------------------")
+print("Resultado: con esto no importan los argumentos\n")

f6d3d8099f4f94c143bedfec5057a524b2553fb5
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-10-30 17:26:36 -0600

    Update codigo-20.py

--- codigo-20.py
+++ codigo-20.py
@@ -1,1 +1,11 @@
-
+print("\nNombre: Ruvalcaba Valverde Miguel Angel")
+print("-------recursida-------")
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+tri_recursion(6)
+#tarde en enteder pero en resumen, es un contador o se puede usar como uno eso enenti yo

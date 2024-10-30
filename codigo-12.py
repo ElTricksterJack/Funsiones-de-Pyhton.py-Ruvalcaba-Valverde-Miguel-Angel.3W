0f7c1fdd752a43d5986560abab1630aef0ac8eb6
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-10-30 17:15:29 -0600

    Update codigo-12.py

--- codigo-12.py
+++ codigo-12.py
@@ -1,1 +1,7 @@
-
+def void():
+  pass
+#las funsiones que estan basias tiene que tener la palabra pass para que no alla errors. =)
+#uf estoy muy cansaditto, pero tengo que continuar o sino sere un fracasaditto, y si pasa eso estar muy agutaditto.
+#ademas no quiero estar despediditto, y eso me estaria estar procupaditto, pero estoy muy emosinaditto y sorprendiditto.
+#hasi que continuare sin estar estresaditto... bueno ese no fue tan grasisos pero hay un pokemon que se llama ditto.
+print("aqui no hay nada *eco* *eco mas bajo* *eco aun mas bajo* ")

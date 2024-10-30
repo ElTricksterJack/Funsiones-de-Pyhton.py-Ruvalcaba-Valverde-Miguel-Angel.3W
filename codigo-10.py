d8d6b3d10f0bd231f10d1ce10a2f4e4da2a73554
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-10-30 17:12:41 -0600

    Update codigo-10.py

--- codigo-10.py
+++ codigo-10.py
@@ -1,1 +1,8 @@
-
+print("\nNombre: Ruvalcaba Valverde Miguel Angel")
+print("----Pasar una lista como argumento----")
+def menu(comida):#vera con esto lo que hasemos es que integramos una lista a nuestra funsion
+  for x in comida:
+    print(x)
+print("--los platillos exoticos son--")
+exotico = ["Estofado de Dinosauro", "Costilla frita de Dragon", "Pizza Quimera"]
+menu(exotico)

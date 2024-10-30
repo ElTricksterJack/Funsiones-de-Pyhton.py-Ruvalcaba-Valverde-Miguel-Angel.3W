7c4955b99ab62f83803c1f14da070f2254078590
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-10-30 17:13:57 -0600

    Update codigo-11.py

--- codigo-11.py
+++ codigo-11.py
@@ -1,1 +1,8 @@
-
+print("\nNombre: Ruvalcaba Valverde Miguel Angel")
+print("----------Valores de retorno----------")
+def por(x):#Para permitir que una función devuelva un valor, utilice la return declaración
+    return 5 * x
+#en otras palabras, para que se muestre el valor de la funsion usa return, y hasi te ahoras la moslestia de haser variables. dandadan
+print(por(3))
+print(por(5))
+print(por(9))

b09af224dbcfd32bf68cef508325f42224f5d8e4
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-10-30 16:55:10 -0600

    Update codigo-2.py

--- codigo-2.py
+++ codigo-2.py
@@ -1,1 +1,13 @@
-
+print("\nNombre: Ruvalcaba Valverde Miguel Angel")
+print("--------------Ejemplo-----------------")
+print("--Imagina una funsion de esta manera--")
+print("Funasion: Ritual antiguo para invocar demonios.")
+print("Codigo de la Funsion: Demonio que se puede invocar.")
+print("Para invocar invocar al demonio Reddoticantel, tienes que desir las palabras antiguas del ritual.")
+def kilaquezoxt():
+    print("El demonio Reddoticantel fue invocado")
+print("kilaquezoxt")
+kilaquezoxt()
+print("y el demonio fue invocado, ese es el ejemplo. =P")
+print("--------------------------------------")
+print("Resultado: \n")

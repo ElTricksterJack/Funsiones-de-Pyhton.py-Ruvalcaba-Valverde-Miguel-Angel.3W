037a4d780e8ff5a178ccdb07c26edfd669114f02
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-10-30 16:59:17 -0600

    Update codigo-4.py

--- codigo-4.py
+++ codigo-4.py
@@ -1,1 +1,9 @@
-
+print("\nNombre: Ruvalcaba Valverde Miguel Angel")
+print("-------------2 Argumentos---------------")
+def fname(nombre, apellido):
+  print(nombre + " " + apellido)
+fname("Nikola", "Tesla")
+#fname("Nikola") #recuerda que si tienes 2 funsones, no puedes dejar una variable basia. amenos que... continuara dandadan
+#Puedes haser mas de un argumento.
+print("--------------------------------------")
+print("Resultado: se bieron los nombre cabiados\n")

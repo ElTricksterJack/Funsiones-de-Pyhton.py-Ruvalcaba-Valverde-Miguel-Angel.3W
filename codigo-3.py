c0c82f082bebb8624baf1e8500dd079bf6ed6c50
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-10-30 16:58:28 -0600

    Update codigo-3.py

--- codigo-3.py
+++ codigo-3.py
@@ -1,1 +1,11 @@
-
+print("\nNombre: Ruvalcaba Valverde Miguel Angel")
+print("-------------Argumentos---------------")
+def dreemurr(fname):
+  print(fname + " Dreemurr")
+#Esto se le llama argumentos, son varibles echas junto a la funsion y estos pueden ser cambiados por bariables ya establesidas.
+dreemurr("Toriel")
+dreemurr("Asgore")
+dreemurr("Asriel")
+dreemurr("Kris")
+print("--------------------------------------")
+print("Resultado: se bieron los nombre cabiados\n")

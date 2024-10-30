fa8b712d4acba2ee71542ed7d4d24c0101fc7529
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-10-30 17:09:37 -0600

    Update codigo-8.py

--- codigo-8.py
+++ codigo-8.py
@@ -1,1 +1,7 @@
-
+print("\nNombre: Ruvalcaba Valverde Miguel Angel")
+print("----Argumentos de palabras clave arbitrarias, **kwargs----")
+def my_function(**kid):
+  print("Mi apellido es " + kid["ape"])
+#Aqui puedes escojer variables clave desde fuera o escribirlas desde afuera de la funsion.
+my_function(name = "Toby", ape = "Fox")
+print("Resultado: se escojio la variable corepta.\n")

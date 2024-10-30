433ca430c0cd99de84c1f7e0971961ac60b817ea
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-10-30 17:20:36 -0600

    Update codigo-16.py

--- codigo-16.py
+++ codigo-16.py
@@ -1,1 +1,11 @@
-
+print("\nNombre: Ruvalcaba Valverde Miguel Angel")
+print("-------Argumentos basados ​​únicamente en palabras clave-------")
+def NotFun(*, x):
+  print(x)
+NotFun(x = 3)
+#Para especificar que una función solo puede tener argumentos de palabras clave, agregue *, antes de los argumentos
+print("\Comparen señores/")
+def NotFunNotFun(x):
+  print(x)
+#Sin él, *, se permite utilizar argumentos posicionales incluso si la función espera argumentos de palabras clave
+NotFunNotFun(3)

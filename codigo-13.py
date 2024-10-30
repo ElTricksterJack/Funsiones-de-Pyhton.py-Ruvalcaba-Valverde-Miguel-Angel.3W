cc1314f308556cd3578c0c3925456c2a7a4527bc
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-10-30 17:16:20 -0600

    Update codigo-13.py

--- codigo-13.py
+++ codigo-13.py
@@ -1,1 +1,12 @@
-
+print("\nNombre: Ruvalcaba Valverde Miguel Angel")
+print("-------Argumentos exclusivamente posicionales-------")
+def fun(x, /):
+  print(x)
+#Puede especificar que una función puede tener SÓLO argumentos posicionales o SÓLO argumentos de palabras clave.
+#Para especificar que una función solo puede tener argumentos posicionales, agregue , / después de los argumentos
+fun(6)
+print("\Comparen señores/")
+#Sin embargo, , /en realidad se permite usar argumentos de palabras clave incluso si la función espera argumentos posicionales
+def funfun(x):
+  print(x)
+funfun(x = 3)

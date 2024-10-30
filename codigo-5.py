3300b95180b79e2d78de441c271f8fddd3f001a0
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-10-30 17:03:18 -0600

    Update codigo-5.py

--- codigo-5.py
+++ codigo-5.py
@@ -1,1 +1,7 @@
+print("\nNombre: Ruvalcaba Valverde Miguel Angel")
+print("-------------2 Argumentos---------------")
+def my_function(negador, name):
+  print(negador + " " + name)
 
+my_function("unseen","sean")
+print("--------------------------------------")
